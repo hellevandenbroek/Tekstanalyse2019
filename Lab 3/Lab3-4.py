d78ea681808575612228bbca3a8c5cdb440f1771
hellevandenbroek <hellevandenbroek@hotmail.com>
2019-02-26 18:13:26 +0100

    adds function for vectorizing

--- Lab 3/Lab3-4.py
+++ Lab 3/Lab3-4.py
@@ -1,10 +1,6 @@
-import tweepy
 from nltk import word_tokenize
 from nltk.corpus import stopwords
 from sklearn.feature_extraction.text import TfidfVectorizer
-
-
-
 
 def readFromFile():
     f = open("twitterCorpus.txt", "r", encoding="utf-8")
@@ -18,6 +14,14 @@ def readFromFile():
         tweets.append(tweeter[tweeter.index(":") + 1:])
     return tweets, names
 
+def vectorize():
+    corpus = ["This is very strange",
+              "This is very nice"]
+    vectorizer = TfidfVectorizer(min_df=1)
+    X = vectorizer.fit_transform(corpus)
+    idf = vectorizer.idf_
+    print(dict(zip(vectorizer.get_feature_names(), idf)))
+
 
 results = readFromFile()
 tweets = results[0]
@@ -26,10 +30,4 @@ names = results[1]
 print(names[0], ': ', tweets[0])
 print(names[1], ': ', tweets[1])
 
-vectorizer = TfidfVectorizer(sublinear_tf=True, stop_words='english')
-corpus = ["This is very strange",
-          "This is very nice"]
-vectorizer = TfidfVectorizer(min_df=1)
-X = vectorizer.fit_transform(corpus)
-idf = vectorizer.idf_
-print (dict(zip(vectorizer.get_feature_names(), idf)))
+vectorize(tweets)
