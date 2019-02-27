543eaf0e0d557f751374cb1551b440a1f0098a84
trulsabe <trulsabe@stud.ntnu.no>
2019-02-27 10:32:28 +0100

    skriv noe

--- Lab 3/Lab3-4.py
+++ Lab 3/Lab3-4.py
@@ -16,23 +16,46 @@ def readFromFile():
         tweets.append(tweeter[tweeter.index(":") + 1:])
     return tweets, names
 
+
 def count_vectorizer(corpus):
-    print(corpus)
+    list_tweets = []
+    for tweet in corpus:
+        list_tweets.append(' '.join(tweet))
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(list_tweets)
+    print(vectorizer.get_feature_names())
+    return(X.toarray())
+
+
+def count_vec_example():
+    corpus = [
+        'This is the first document.',
+         'This is the second second document.',
+         'And the third one.',
+         'Is this the first document?'
+    ]
     vectorizer = CountVectorizer()
     X = vectorizer.fit_transform(corpus)
+    print(vectorizer.get_feature_names())
     return(X.toarray())
+
 
 results = readFromFile()
 tweets = results[0]
 names = results[1]
 
-print('\n---------------CORPUSES---------------')
+print('\n---------------TWEETS---------------')
+print("tweets:", tweets)
+
+print('\n---------------CORPORA---------------')
 print(names[5], ': ', tweets[5])
 print(names[1], ': ', tweets[1])
 
 print('\n---------------VECTORS1---------------')
-print(count_vectorizer(tweets[5]))
+# print(count_vectorizer(tweets))
 print('\n---------------VECTORS2---------------')
-print(count_vectorizer(tweets[1]))
+# print(count_vectorizer(tweets))
 
 # vectorize(tweets)
+
+print(count_vec_example())
