29eb332fab148e37da02cf7498234c53a886e5f0
Helle van den Broek <hvbroek@stud.ntnu.no>
2019-02-27 10:01:54 +0100

    Adds vectorizing function

--- Lab 3/Lab3-4.py
+++ Lab 3/Lab3-4.py
@@ -16,29 +16,22 @@ def readFromFile():
         tweets.append(tweeter[tweeter.index(":") + 1:])
     return tweets, names
 
-
-def tf_idf_vectorize():
-    corpus = ["This is very strange",
-              "This is very nice"]
-    vectorizer = TfidfVectorizer(min_df=1)
-    X = vectorizer.fit_transform(corpus)
-    idf = vectorizer.idf_
-    print(dict(zip(vectorizer.get_feature_names(), idf)))
-
 def count_vectorizer(corpus):
-    vec = CountVectorizer()
-    transf = vec.fit_transform(corpus)
-    print(vec.get_feature_names())
-    return transf
+    vectorizer = CountVectorizer()
+    X = vectorizer.fit_transform(corpus)
+    return(X.toarray())
 
 results = readFromFile()
 tweets = results[0]
 names = results[1]
-print(names[0])
-print("00:", results[0][0])
 
+print('\n---------------CORPUSES---------------')
 print(names[0], ': ', tweets[0])
 print(names[1], ': ', tweets[1])
-print(count_vectorizer(results[0][1]))
+
+print('\n---------------VECTORS1---------------')
+print(count_vectorizer(tweets[0]))
+print('\n---------------VECTORS2---------------')
+print(count_vectorizer(tweets[1]))
 
 # vectorize(tweets)
