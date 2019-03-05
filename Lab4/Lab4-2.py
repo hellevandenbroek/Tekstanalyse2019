07f070f1abab3dd53d0f97782261da40f2693bbc
trulsabe <trulsabe@stud.ntnu.no>
2019-03-05 15:51:52 +0100

    Takk for at du hadde riktig navn på mappa. Added ngram to ex2

--- Lab4/Lab4-2.py
+++ Lab4/Lab4-2.py
@@ -1,8 +1,19 @@
+from nltk import ngrams
 
+
+
+"""
+    TODO: 
+    - find out how to generate full sentence using nltk's ngrams
+    - how ngram works
+    http://www.nltk.org/_modules/nltk/model/ngram.html
+    - feature for input 
+    - return most correct sentence and its probability
+"""
 
 def readFromFile():
     # trying without SW
-    f = open("twitterCorpusSW.txt", "r", encoding="utf-8")
+    f = open("twitterCorpus.txt", "r", encoding="utf-8")
     tweets = []
     names = []
     for line in f.readlines():
@@ -14,4 +25,29 @@ def readFromFile():
         tweets.append(tweeter[colon + 1:])
     return tweets, names
 
+
+def split_tweets(tweets):
+    list_tweets = []
+    for line in tweets:
+        for word in line.split():
+            list_tweets.append(word)
+    return list_tweets
+
+
+def start_ngram():
+    tweets = readFromFile()[0][0]  # tweets > trump's
+    print(tweets)
+    spl = split_tweets(tweets)
+    model = ngrams(spl, 3)
+
+    prob = find_probability(model, "and", tweets)
+    print(prob)
+    return model
+
+
+def find_probability(ngram, word, context):
+    return ngram.probdi(word, context)
+
+
+ng = start_ngram()
 
