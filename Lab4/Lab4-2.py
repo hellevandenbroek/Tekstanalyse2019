adb24da9b36a6548276a0dce32a5b773a556080d
hellevandenbroek <hellevandenbroek@hotmail.com>
2019-03-08 21:43:34 +0100

    Added more stuff to 4-2

--- Lab4/Lab4-2.py
+++ Lab4/Lab4-2.py
@@ -24,23 +24,6 @@ def readFromFile():
         tweets.append(tweeter[colon + 1:])
     return tweets, names
 
-
-def split_tweets(tweets):
-    list_tweets = []
-    for line in tweets:
-        for word in line.split():
-            list_tweets.append(word)
-    return list_tweets
-
-
-def start_ngram():
-    tweets = readFromFile()[0][0]  # tweets > trump's
-    spl = split_tweets(tweets)
-    model = ngrams(spl, 3)
-    prob = find_probability(model, "and", tweets)
-    return model
-
-
 def find_probability(ngram, word, context):
     #return ngram.probdi(word, context)
     return 'hello'
@@ -52,12 +35,10 @@ def start_gram(tweets):
     for grams in sixgrams:
         print(grams)
 
-tweet_list = readFromFile()[0]
-print(tweet_list)
-str1 = ''.join(str(e) for e in tweet_list)
-str2 = ''.join(str1)
-print(str1)
-print(str2)
-ng = start_gram(str2)
+tweets = readFromFile()[0]
+print(tweets)
+list_tweets = ''.join(str(e) for e in tweets)
+print(list_tweets)
+ng = start_gram(list_tweets)
 
 
