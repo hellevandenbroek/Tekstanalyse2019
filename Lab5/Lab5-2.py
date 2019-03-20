31542feb6c55854856838c799c6964eb49424cfa
trulsabe <trulsabe@stud.ntnu.no>
2019-03-20 11:17:57 +0100

    Add Lab5-2 to the list of started-on exercises bla

---
+++ Lab5/Lab5-2.py
@@ -0,0 +1,23 @@
+from nltk import sem
+
+
+read_expr = sem.Expression.fromstring
+a = read_expr('P -> not Q')
+
+b = read_expr('P and Q')
+
+c = read_expr('-P -> Q')
+
+d = read_expr('(P or Q) -> -R')
+
+e = read_expr('-(P | R)')
+
+f = read_expr('(P -> -R) <-> (Q -> -S)')
+
+tasks = [a, b, c, d, e, f]
+
+for task in tasks:
+    
+    print(task)
+
+
