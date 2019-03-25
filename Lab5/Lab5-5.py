094a46189f3267bbf2ead8aa86ccdfd869caa384
Helle van den Broek <hvbroek@stud.ntnu.no>
2019-03-25 09:52:35 +0100

    Added solutions for ex 5

--- Lab5/Lab5-5.py
+++ Lab5/Lab5-5.py
@@ -1,6 +1,11 @@
+from keras.layers import Dense, Activation
+from keras.models import Sequential
 from textblob import TextBlob
 
+
+
 # Sources: https://medium.freecodecamp.org/how-to-build-a-twitter-sentiments-analyzer-in-python-using-textblob-948e1e8aae14
+
 
 def readFromFile():
     f = open("twitterCorpus.txt", "r", encoding="utf-8")
@@ -10,15 +15,30 @@ def readFromFile():
         tweets.append(tweet)
     return tweets
 
-all_tweets = readFromFile()
 
-for tweet in all_tweets:
-    print(tweet)
-    analysis = TextBlob(tweet)
-    print(analysis.sentiment)
-    if analysis.sentiment[0] > 0:
-        print('Positive')
-    elif analysis.sentiment[0] < 0:
-        print('Negative')
-    else:
-        print('Neutral')
+def analyseSentiment(tweets):
+    for tweet in tweets:
+        print(tweet)
+        analysis = TextBlob(tweet)
+        print(analysis.sentiment)
+        if analysis.sentiment[0] > 0:
+            print('Positive')
+        elif analysis.sentiment[0] < 0:
+            print('Negative')
+        else:
+            print('Neutral')
+
+allTweets = readFromFile()
+analyseSentiment(allTweets)
+
+#Source https://keras.io/getting-started/sequential-model-guide/
+
+model = Sequential()
+model = Sequential([
+    Dense(32, input_shape=(784,)),
+    Activation('relu'),
+    Dense(10),
+    Activation('softmax'),
+])
+
+print(allTweets)
