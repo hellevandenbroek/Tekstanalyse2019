4162899e2237d6188c6a20af0c10db5fcc7b8396
Helle van den Broek <hvbroek@stud.ntnu.no>
2019-03-25 11:45:37 +0100

    Started working on project

---
+++ Project/index.py
@@ -0,0 +1,10 @@
+#TODO: make corpus great again. Get 200 tweets from input user
+#TODO: treat corpus
+#TODO: Ngrams, få dette til å bli riktig
+#TODO: poetry generation
+
+import makeCorpus
+
+
+user = input("Type in a twitter-account you want to generate poetry from: ")
+makeCorpus.make_corpus(user)
