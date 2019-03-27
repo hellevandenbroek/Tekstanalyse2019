9102d57b28a266548a7bd30e5d460b16f9d18ac8
trulsabe <trulsabe@stud.ntnu.no>
2019-03-27 14:49:57 +0100

    Added list option to UI

--- Project/app.py
+++ Project/app.py
@@ -6,27 +6,43 @@
 import sys
 from twitterCorpus import CorpusGenerator
 from poetryGenerator import PoetryGenerator
-
-options = ["add account", "generate poem", "exit application"]
-
+import os
+options = ["add account",
+           "generate poem",
+            "list corpus",
+           "exit application"]
 
 def print_options():
     for option in options:
         capitalized = option.split()
         first_word = ''.join([x.capitalize() for x in capitalized[0]])
         rest = ''.join(capitalized[1:])
-        print("- {} {}".format(first_word, rest))
+        print("\t- {} {}".format(first_word, rest))
 
 
 def determine_choice(choice):
     for i in range(len(options)):
         option = options[i]
+        if choice == option:
+            return i
         split_option = option.split()
         for spl in split_option:
             if choice == spl:
                 return i
     print("Argument invalid. Try again.")
     return -1
+
+
+def handle_list():
+    files = os.listdir("./Corpus")
+    shaved = [x.strip(".txt") for x in files]
+    print_accounts(shaved)
+
+
+def print_accounts(accounts):
+    print("Generate poems based on these accounts:")
+    for account in accounts:
+        print("\t- {}".format(account))
 
 
 def handle_corpus():
@@ -41,7 +57,7 @@ def handle_poem_generation():
     hello = PoetryGenerator(user)
     hello.make_chunks()
     hello.print_poem()
-    print("Want to save latest poem to disk? y/n")
+    print("Want to save this poem to disk? y/n")
     save = input(">")
     if save.startswith("y"):
         hello.save_poem()
@@ -61,5 +77,7 @@ while True:
         if chosen_choice == 1:
             handle_poem_generation()
         if chosen_choice == 2:
+            handle_list()
+        if chosen_choice == 3:
             print("Closed application")
             sys.exit()
