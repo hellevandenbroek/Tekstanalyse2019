55056864a3b548ebf9dbdb5f5452d7f22f61d3b5
trulsabe <trulsabe@stud.ntnu.no>
2019-03-28 15:37:38 +0100

    Split PoetryGenerator to Base and actual generator #15

--- Project/poetryGenerator.py
+++ Project/poetryGenerator.py
@@ -4,12 +4,7 @@ import pathlib
 from textblob import TextBlob
 
 
-class PoetryGenerator:
-    """
-        This class contains the creation of the
-        poems. This makes use of the files from
-        the Corpus directory.
-    """
+class BasePoetryGenerator:
     def __init__(self, username, mood):
         self.foo = "bar"
         self.username = username
@@ -20,7 +15,19 @@ class PoetryGenerator:
         self.wrb = []
         self.poem = ""
 
-        pathlib.Path('Poems').mkdir(exist_ok=True)
+    def print_poem(self):
+        print("\n Based of: \n", self.username, "\n")
+        print(self.poem, "\n")
+
+    def save_poem(self):
+        if self.poem == "":
+            return
+        try:
+            file = open("Poems/{}.txt".format(self.username.lower()), "ab")
+            file.write(self.poem.encode() + '\n\n'.encode())
+        finally:
+            file.close()
+            print("Added poem to collection of {} ".format(self.username))
 
     def fetch_tweets(self):
         print('Now generating a {} poem based on the tweets from {}......'.format(self.mood, self.username))
@@ -29,6 +36,23 @@ class PoetryGenerator:
         for line in file:
             corpus.append(line)
         self.corpus = corpus
+
+
+class PoetryGenerator(BasePoetryGenerator):
+    """
+        This class contains the creation of the
+        poems. This makes use of the files from
+        the Corpus directory.
+    """
+    def __init__(self, username, mood):
+        super(PoetryGenerator, self).__init__(username, mood)
+        self.corpus = []
+        self.np = []
+        self.clause = []
+        self.wrb = []
+        self.poem = ""
+
+        pathlib.Path('Poems').mkdir(exist_ok=True)
 
     def make_chunks(self):
         document = self.corpus
@@ -104,20 +128,6 @@ class PoetryGenerator:
         fourth_line = self.np[random.randint(0, lenNP)]
         self.poem = "{}\n{}\n{}\n{}".format(first_line, second_line, third_line, fourth_line)
         self.print_poem()
-
-    def print_poem(self):
-        print("\n Based of: \n", self.username, "\n")
-        print(self.poem, "\n")
-
-    def save_poem(self):
-        if self.poem == "":
-            return
-        try:
-            file = open("Poems/{}.txt".format(self.username.lower()), "ab")
-            file.write(self.poem.encode() + '\n\n'.encode())
-        finally:
-            file.close()
-            print("Added poem to collection of {} ".format(self.username))
 
 
 # Part of speech cheat note:
